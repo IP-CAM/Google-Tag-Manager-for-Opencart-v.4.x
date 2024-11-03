0bd60cf4ae4bf4f8ba8a2b83dcbaadca1f887559
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-03 14:05:14 +0100

    codeblock comment

--- src/catalog/controller/analytics/ps_gtm.php
+++ src/catalog/controller/analytics/ps_gtm.php
@@ -9,10 +9,27 @@ class PsGtm extends \Opencart\System\Engine\Controller
 {
     public function index(): string
     {
+        /**
+         * Checks if Google Tag Manager (GTM) is enabled in the configuration.
+         *
+         * If disabled, the function returns an empty string, meaning no GTM script will be added to the page.
+         */
         if (!$this->config->get('analytics_ps_gtm_status')) {
             return '';
         }
 
+        /**
+         * Retrieves GTM and GCM configuration settings from the system configuration.
+         *
+         * - `$gtm_id`: Unique ID for the Google Tag Manager container.
+         * - `$gcm_status`: Boolean indicating if Google Consent Mode (GCM) is enabled.
+         * - `$ad_storage`, `$ad_user_data`, `$ad_personalization`, `$analytics_storage`,
+         *   `$functionality_storage`, `$personalization_storage`, `$security_storage`:
+         *   Booleans determining storage access settings for different types of cookies/data.
+         * - `$wait_for_update`: Integer representing delay (in milliseconds) before applying consent settings.
+         * - `$ads_data_redaction`: Boolean setting for redacting ads data.
+         * - `$url_passthrough`: Boolean to control URL passthrough setting for enhanced link tracking.
+         */
         $gtm_id = $this->config->get('analytics_ps_gtm_gtm_id');
         $gcm_status = (bool) $this->config->get('analytics_ps_gtm_gcm_status');
         $ad_storage = (bool) $this->config->get('analytics_ps_gtm_ad_storage');
@@ -28,6 +45,18 @@ class PsGtm extends \Opencart\System\Engine\Controller
 
         $html = '';
 
+        /**
+         * If Google Consent Mode (GCM) is enabled, configure consent settings.
+         *
+         * Creates a `gcm_options` array, defining consent preferences for storage and data types. These include:
+         * - `ad_storage`, `ad_user_data`, `ad_personalization`, `analytics_storage`,
+         *   `functionality_storage`, `personalization_storage`, `security_storage`:
+         *   Each is set to either "granted" or "denied" based on the corresponding configuration.
+         * - If `$wait_for_update` is greater than 0, it adds a delay before applying these settings.
+         *
+         * Appends a JavaScript block to `$html` that initializes the `dataLayer`, defines the `gtag` function,
+         * and sets consent using the `gcm_options` and `ads_data_redaction` and `url_passthrough` configurations.
+         */
         if ($gcm_status) {
             $gcm_options = [
                 'ad_storage' => $ad_storage ? 'granted' : 'denied',
@@ -53,6 +82,13 @@ class PsGtm extends \Opencart\System\Engine\Controller
             </script>';
         }
 
+        /**
+         * Appends the main GTM script block to `$html`.
+         *
+         * This block creates and inserts a script element into the page that loads GTM from `googletagmanager.com`.
+         * - Uses the GTM ID retrieved from configuration.
+         * - Initializes the dataLayer and sends a 'gtm.js' event to trigger GTM processing.
+         */
         $html .= '
         <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
