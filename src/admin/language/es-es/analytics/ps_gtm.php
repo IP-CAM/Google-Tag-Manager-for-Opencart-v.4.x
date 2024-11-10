9f9ebdcaff72fb80c31a3d3eee21ab646f1beeaa
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-10 15:30:24 +0100

    Espanol language

---
+++ src/admin/language/es-es/analytics/ps_gtm.php
@@ -0,0 +1,69 @@
+<?php
+// Heading
+$_['heading_title']                = 'Playful Sparkle - Google Tag Manager';
+$_['heading_fix']                  = 'Corregir errores comunes';
+$_['heading_getting_started']      = 'Introducción';
+$_['heading_setup']                = 'Configuración de Google Tag Manager';
+$_['heading_troubleshot']          = 'Solución de problemas comunes';
+$_['heading_faq']                  = 'Preguntas frecuentes';
+$_['heading_contact']              = 'Contactar soporte';
+
+// Text
+$_['text_extension']                = 'Extensiones';
+$_['text_edit']                     = 'Editar Google Tag Manager';
+$_['text_success']                  = 'Éxito: ¡Has modificado Google Tag Manager!';
+$_['text_group_ad_settings']        = 'Configuraciones de anuncios';
+$_['text_group_analytics_settings'] = 'Configuraciones de análisis';
+$_['text_group_security_settings']  = 'Configuraciones de seguridad';
+$_['text_group_advanced_settings']  = 'Configuraciones avanzadas';
+$_['text_getting_started']          = '<p><strong>Visión general:</strong> El Playful Sparkle - Google Tag Manager para OpenCart 4.x te permite gestionar y desplegar fácilmente etiquetas de marketing en tu tienda OpenCart sin necesidad de habilidades de programación. Con esta extensión, puedes agilizar la configuración de seguimiento, mejorando la eficiencia en análisis y marketing.</p><p><strong>Requisitos:</strong> Asegúrate de tener una cuenta de Google Tag Manager, un ID de medición válido y estar utilizando una versión compatible de OpenCart 4.x+, PHP 7.4+.</p>';
+$_['text_setup']                    = '<p><strong>Configuración de Google Tag Manager:</strong> Inicia sesión en tu <a href="https://tagmanager.google.com" target="_blank" rel="noopener noreferrer">cuenta de Google Tag Manager</a>, crea un nuevo contenedor para tu sitio web si aún no lo has hecho, y recupera tu ID de medición. Luego, ingresa este ID en el campo designado de la configuración de la extensión Playful Sparkle - Google Tag Manager en tu tienda OpenCart.</p>';
+$_['text_troubleshot']              = '<ul><li><strong>ID de medición no encontrado:</strong> Verifica que hayas ingresado el ID de medición correcto en la configuración de la extensión. Asegúrate de que no haya espacios extra o caracteres incorrectos.</li><li><strong>Las etiquetas no se activan:</strong> Asegúrate de que tus etiquetas estén correctamente configuradas en Google Tag Manager con los desencadenantes correctos. Puedes utilizar <a href="https://tagassistant.google.com/" target="_blank" rel="noopener noreferrer">Google Tag Assistant</a> para solucionar problemas de activación de etiquetas.</li><li><strong>Problemas de permisos:</strong> Asegúrate de tener los permisos necesarios en tu cuenta de Google Tag Manager para crear y gestionar etiquetas.</li><li><strong>Problemas de compatibilidad:</strong> Asegúrate de que tu versión de OpenCart esté actualizada y verifica que no haya otros módulos instalados que estén en conflicto con la configuración de Google Tag Manager.</li></ul>';
+$_['text_faq']                      = '<details><summary>¿Qué hacer si no veo mis etiquetas activándose en Google Tag Manager?</summary>Asegúrate de que el ID de medición sea correcto y verifica que las etiquetas estén publicadas y correctamente configuradas con los desencadenantes adecuados.</details><details><summary>¿Cómo puedo comprobar si la extensión está funcionando?</summary>Utiliza la extensión <a href="https://tagassistant.google.com/" target="_blank" rel="noopener noreferrer">Google Tag Assistant</a> de Chrome o el modo de vista previa en Google Tag Manager para confirmar que tus etiquetas se están activando como se espera.</details><details><summary>¿Es esta extensión compatible con otras extensiones?</summary>Aunque esta extensión es generalmente compatible, se recomienda probarla con otras extensiones instaladas en tu sitio para evitar conflictos. Si encuentras problemas, verifica si existen problemas de compatibilidad conocidos.</details><details><summary>¿Cómo soluciono problemas comunes con las etiquetas?</summary>Consulta la guía de solución de problemas de Google Tag Manager para obtener ayuda con problemas comunes de etiquetas y mejores prácticas para la configuración.</details>';
+$_['text_contact']                  = '<p>Para más asistencia, por favor contacta a nuestro equipo de soporte:</p><ul><li><strong>Contacto:</strong> <a href="mailto:%s">%s</a></li><li><strong>Documentación:</strong> <a href="%s" target="_blank" rel="noopener noreferrer">Documentación del usuario</a></li></ul>';
+
+// Tab
+$_['tab_general']                   = 'General';
+$_['tab_gcm']                       = 'Modo de Consentimiento de Google (GCM)';
+$_['tab_help_and_support']          = 'Ayuda y Soporte';
+
+// Entry
+$_['entry_status']                  = 'Estado';
+$_['entry_gtm_id']                  = 'ID de medición';
+$_['entry_gcm_status']              = 'Habilitar GCM';
+$_['entry_ad_storage']              = 'Almacenamiento de anuncios';
+$_['entry_ad_user_data']            = 'Datos de usuario de anuncios';
+$_['entry_ad_personalization']      = 'Personalización de anuncios';
+$_['entry_analytics_storage']       = 'Almacenamiento de análisis';
+$_['entry_functionality_storage']   = 'Almacenamiento de funcionalidad';
+$_['entry_personalization_storage'] = 'Almacenamiento de personalización';
+$_['entry_security_storage']        = 'Almacenamiento de seguridad';
+$_['entry_wait_for_update']         = 'Esperar actualización';
+$_['entry_ads_data_redaction']      = 'Redacción de datos de anuncios';
+$_['entry_url_passthrough']         = 'Transparencia de URL';
+$_['entry_strict']                  = 'Estricto';
+$_['entry_balanced']                = 'Balanceado';
+$_['entry_custom']                  = 'Personalizado';
+$_['entry_gcm_profiles']            = 'Perfiles GCM';
+
+// Button
+$_['button_fix_event_handler']      = 'Corregir manejador de eventos';
+
+// Help
+$_['help_ad_storage']               = 'Controla si se permite el almacenamiento de datos con fines relacionados con anuncios, como el seguimiento de clics en anuncios o conversiones.';
+$_['help_ad_user_data']             = 'Determina si se almacenan datos sobre los usuarios que interactúan con los anuncios, mejorando las capacidades de segmentación de anuncios.';
+$_['help_ad_personalization']       = 'Permite la personalización de anuncios en función de los datos del usuario, proporcionando anuncios más relevantes.';
+$_['help_analytics_storage']        = 'Permite el almacenamiento de datos utilizados para fines de análisis, ayudando a rastrear el rendimiento del sitio y el comportamiento de los usuarios.';
+$_['help_functionality_storage']    = 'Permite el almacenamiento de datos para soportar funcionalidades como preferencias del usuario o características del sitio que mejoran la experiencia.';
+$_['help_personalization_storage']  = 'Controla el almacenamiento de datos para personalizar la experiencia del usuario, como contenido o configuraciones recomendadas.';
+$_['help_security_storage']         = 'Asegura el almacenamiento de datos relacionados con la seguridad, como prevención de fraudes y control de acceso seguro.';
+$_['help_wait_for_update']          = 'Establece el tiempo (en milisegundos) para retrasar la actualización del estado de consentimiento, asegurando que se apliquen todas las configuraciones.';
+$_['help_ads_data_redaction']       = 'Redacta los datos de los usuarios relacionados con anuncios, garantizando la privacidad al ocultar información identificable.';
+$_['help_url_passthrough']          = 'Permite que la URL pase por las comprobaciones de consentimiento, útil para rastrear rutas de usuarios específicas sin almacenar datos personales.';
+$_['help_gcm_status']               = 'Habilita el Modo de Consentimiento de Google, permitiendo que tu sitio ajuste el comportamiento de las etiquetas de Google según la configuración de consentimiento del usuario. Este modo proporciona un seguimiento amigable con la privacidad, permitiendo que los análisis y anuncios funcionen de acuerdo con las preferencias de consentimiento.';
+$_['help_gtm_id_locate']            = 'Para encontrar tu ID de medición, inicia sesión en tu <a href="https://tagmanager.google.com" target="_blank" rel="external noopener noreferrer">cuenta de Google Tag Manager</a>. En el espacio de trabajo, busca el ID en la parte superior del panel: comienza con "GTM-" seguido de una serie única de letras y números, como "GTM-XXXXXXX". <a href="https://support.google.com/analytics/answer/12270356?hl=es" target="_blank" rel="external noopener noreferrer">Más instrucciones detalladas aquí</a>.';
+
+// Error
+$_['error_permission']              = 'Advertencia: ¡No tienes permiso para modificar la configuración de Google Tag Manager!';
+$_['error_gtm_id']                  = 'El campo ID de GTM es obligatorio. Por favor, ingresa tu ID de medición.';
+$_['error_gtm_id_invalid']          = 'El formato del ID de GTM es incorrecto. Asegúrate de que siga el formato GTM-XXXXXXX.';
